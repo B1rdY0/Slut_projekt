22b352280fe3d47e1652fafb7166ad144be029e5
Aaryan hegde <hegde.aaryan@gmail.com>
2022-05-06 18:07:21 +0200

    eefefe

--- Slut_projekt/Slut_projekt/Program.cs
+++ Slut_projekt/Slut_projekt/Program.cs
@@ -1,125 +1,207 @@
 ﻿using System;
 using System.Threading;
 
+string startAgain = "yes";
 
-string name = "";                               //man skriver sitt namn
-RoomNames currentRoom = RoomNames.Start;        // skapar variabler för att göra dem olika rummen
-
-
-
-while (name.Length <= 1 || name.Length > 12)            //kollar att namnen inte ska vara mindre eller längre
-{                                                       // än 1 och 12 bokstäver eller number
-    Console.WriteLine($"Type your name here:");
-    name = Console.ReadLine();
-    if (name.Length <= 2)
-    {
-        Console.WriteLine("your name is too Short");
-        Console.WriteLine("please type again");
-    }
-
-    else if (name.Length > 12)
-    {
-        Console.WriteLine("Your name is too Long");
-        Console.WriteLine("Please type again");
-    }
-
-}
-
-
-
-int PLHP = 100;
-int Boss1HP = 500;
-ToolBox.printStats(PLHP, Boss1HP);          //
-string answer = "Sword";
-answer = Console.ReadLine();
-answer.ToLower();
-
-
-
-while (currentRoom != RoomNames.End)
+while (startAgain == "yes")     //Om du ska starta igen så börjar du från början
 {
-    if (currentRoom == RoomNames.Start)
-    {
 
-        Console.WriteLine($"Welcome to Room1 {name}");
-        Thread.Sleep(1000);
-        Console.WriteLine("");
-        Console.WriteLine($"There is going to be a choice {name}");
-        Thread.Sleep(1000);
-        Console.WriteLine("");
-        Console.WriteLine("Please choose the right answer");
-        Thread.Sleep(1000);
-        Console.WriteLine("");
-        Console.WriteLine("what is better: 1. touch grass  2. play Leauge of Legends");
-        string action = Console.ReadLine();
+    string name = "";                               //man skriver sitt namn
+    RoomNames currentRoom = RoomNames.Start;        // skapar variabler för att göra dem olika rummen
 
+    Console.WriteLine("Please press enter when it stops coming text");
 
-        if (action == "1")
+    while (name.Length <= 1 || name.Length > 12)            //kollar att namnen inte ska vara mindre eller längre
+    {                                                       // än 1 och 12 bokstäver eller number
+        Console.WriteLine($"Type your name here:");
+        name = Console.ReadLine();
+
+        if (name.Length <= 2 || name.Length >= 1)
         {
-            currentRoom = RoomNames.Arena;
+            Console.WriteLine("your name is too Short");
+            Console.WriteLine("please type again");
         }
 
-        if (action == "2")
+        if (name.Length < 1)
         {
-            currentRoom = RoomNames.Unworthy;
+            Console.WriteLine("Please enter a valid name");
         }
 
-    }
-
-    if (currentRoom == RoomNames.Unworthy)
-    {
-        Console.WriteLine($"Bruh you are not worthy of playing the game, please QUIT GAME NOW {name}!!!!");
-
-    }
-
-
-    if (currentRoom == RoomNames.Arena)
-    {
-        answer = "";
-        while (Boss1HP != 0 && PLHP != 0)
+        else if (name.Length > 12)
         {
+            Console.WriteLine("Your name is too Long");
+            Console.WriteLine("Please type again");
+        }
+    }
 
-            while (answer != "1" && answer != "2" && answer != "3")
+    int CashMoney = 0;
+    int Bank = 0;
+    int PLHP = 100;
+    int Boss1HP = 500;
+    string answer = "Sword";
+    answer = Console.ReadLine();      //Svart blir vad du skriver
+    answer.ToLower();                  //
+
+
+
+    while (currentRoom != RoomNames.End)                           // När den ska börja om så börjar den här
+    {
+        if (currentRoom == RoomNames.Start)                        // Första rummen 
+        {
+            ToolBox.printStats(PLHP, Boss1HP);          // Metod som ändrar player HP och Boss HP
+            Console.WriteLine("");
+            Console.WriteLine($"Welcome to the first room {name}");
+            Thread.Sleep(1000);
+            Console.WriteLine("");
+            Console.WriteLine($"There is going to be a choice {name}");
+            Thread.Sleep(1000);
+            Console.WriteLine("");
+            Console.WriteLine("Please choose the right answer");
+            Thread.Sleep(1000);
+            Console.WriteLine("");
+            Console.WriteLine("what is better: 1. touch grass  2. play Leauge of Legends");
+            string action = Console.ReadLine();
+
+
+            if (action == "1")                         // Säger vad den ska gå efter man har vallt 1
             {
-                Console.WriteLine($"Please choose a weapon {name}");
+                currentRoom = RoomNames.Arena;          // Arena där första bossen är 
+            }
+
+            if (action == "2")
+            {
+                currentRoom = RoomNames.Unworthy;       // hamnar i ett rum där spelet stängs av 
+            }
+
+        }
+
+        //Vad som händer när du väljer 2
+        else if (currentRoom == RoomNames.Unworthy)
+        {
+            Console.WriteLine($"Bruh you are not worthy of playing the game, please QUIT GAME NOW {name}!!!!");
+
+        }
+
+        //Arenan 
+        else if (currentRoom == RoomNames.Arena)
+        {
+            while (Boss1HP > 0 && PLHP > 0)   //Om bossen och Playern är inte 0 så börjar while loopen
+            {
+                answer = "";                 //Vad du skriver blir variablen
+
+                while (answer != "1" && answer != "2" && answer != "3")      // Du måste välja en av 1,2 eller 3 för att komma vidare annars loopar frågan
+                {
+                    Console.WriteLine($"Please choose a weapon {name}");
+                    Console.WriteLine("");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("please choose:   1: Sword   2: Mace   3: Scythe");
+                    answer = Console.ReadLine();
+
+                }
+
+
+                if (answer == "1")                  //När man väljer 1
+                {
+                    (Boss1HP, PLHP) = ToolBox.Sword(Boss1HP, PLHP);  //Byter värdet på Boss HP och Player HP i Metoden
+                }
+
+
+                if (answer == "2")
+                {
+                    (Boss1HP, PLHP) = ToolBox.Mace(Boss1HP, PLHP);
+                }
+
+                if (answer == "3")
+                {
+                    (Boss1HP, PLHP) = ToolBox.Scythe(Boss1HP, PLHP);
+                }
+
+                else           //Annars så stängs spelet
+                {
+                    Console.ReadLine();
+                }
+
+            }
+
+            Bank += 1000;              //hur mycket pengar du har i banken 
+
+            //när bossen är död så ska den stora If funktionen starta
+            if (Boss1HP <= 0)
+            {
+
+                bool Broke = false;       // Om man är inte är fattig 
+
+                while (Broke != true)               // om du är inte är fattig så ska loopen starta
+                {
+                    Console.WriteLine($"you have {Bank} in the bank");
+                    Console.WriteLine("You have to cash out the money to buy the items");
+                    Console.WriteLine("please cash out any number between 200 - 500");
+                    string input = Console.ReadLine();
+
+                    Broke = int.TryParse(input, out CashMoney);             //Hur mycket du sätter in mellan 0 -1000 byter, int till en string 
+
+                    if (CashMoney > 500 || CashMoney < 200)                  //Vad som händer när du inte fyller kriterierna 
+                    {
+                        Console.WriteLine("Sorry not possible for cash out");
+                        Thread.Sleep(1500);
+                        Console.WriteLine("You typed a number too high or too low");
+                        Console.ReadLine();
+
+                    }
+
+                    else if (CashMoney < 500 && CashMoney > 200)         // Vad som händer när du fyller kriterierna, altså du går vidare till shop
+                    {
+                        Console.WriteLine("Welcome to the shop");
+                        currentRoom = RoomNames.Shop;    //Byter rum 
+
+                    }
+                }
+
+            }
+
+
+            if (currentRoom == RoomNames.Shop)        //Vad som händer i den här rummen (Shop)
+            {
+
+                Console.WriteLine("There is 5 items and you can only afford a 1 item");
+                Thread.Sleep(1500);
+                Console.WriteLine("please type in the number 1, the other number are out of stock at the moment ");
                 Console.WriteLine("");
-                Thread.Sleep(1000);
-                Console.WriteLine("please choose:   1: Sword   2: Mace   3: Scythe");
-                answer = Console.ReadLine();
+                Console.WriteLine("please press enter to continue");
+                Console.WriteLine("The options are");
+                Console.WriteLine("1:                   Drugs: Price is 400");
+                Console.WriteLine("2:                   steroids: Price is 300 ");
+                Console.WriteLine("3:                   Dark heart: Price is 500");
+                Console.WriteLine("4:                   Sword of the Gods: Price is 500");
+                Console.WriteLine("5:                   Eternal youth: Price is 400");
+
+                string answerItem;
+                answerItem = Console.ReadLine();                //tar svaren som är given på alternativen uppe
+                answerItem = answerItem.ToLower();
+
+                if (answerItem == "1")                  // Startar en funktion där du går till nästa level(rum)
+                {
+                    Console.WriteLine("Then please proceed to the next level");
+                    Bank -= 400;    //Tar ut pengr när du har köpt
+                }
 
             }
 
-
-            if (answer == "1")
-            {
-                (Boss1HP, PLHP) = ToolBox.Sword(Boss1HP, PLHP);
-            }
+        }
 
 
-            if (answer == "2")
-            {
-                (Boss1HP, PLHP) = ToolBox.Mace(Boss1HP, PLHP);
-            }
-
-            if (answer == "3")
-            {
-                (Boss1HP, PLHP) = ToolBox.Scythe(Boss1HP, PLHP);
-            }
-
-            else
-            {
-                Console.ReadLine();
-            }
+        if (PLHP <= 0)          //När player dör så kommer du att få starta igen eller sluta 
+        {
+            currentRoom = RoomNames.End;
         }
 
     }
 
+    startAgain = ToolBox.Try();
 
-    Console.ReadLine();
 }
 
-
-enum RoomNames
+enum RoomNames                                      //Skapar egna variabler som är ställen som player kommer vara
 {
-    Start, Arena, Unworthy, End
+    Start, Arena, Unworthy, End, Shop
 };
--- Slut_projekt/Slut_projekt/ToolBox.cs
+++ Slut_projekt/Slut_projekt/ToolBox.cs
@@ -1,31 +1,31 @@
 using System;
 using System.Threading;
 
-
+// Alla metoder 
 
 public class ToolBox
 {
 
     public static void printStats(int playerHealth, int Boss1Health)
     {
-        Console.WriteLine($"you have {playerHealth} HP");
+        Console.WriteLine($"you have {playerHealth} HP");           //Skriver ut både player HP och Boss HP
         Thread.Sleep(100);
         Console.WriteLine($"The boss has {Boss1Health}");
     }
 
-    public static (int, int) Sword(int BossHP, int playerHp)
+    //Dem tre gör samma sak i princip
+    public static (int, int) Sword(int BossHP, int playerHp)   //Metod som säger vad Sword ska göra och vad bossen ska göra
     {
         int PLDMGS1 = 250;
         int BossDMG = 80;
 
 
-        Random generator = new Random();
+        Random generator = new Random();    //Gör en chance om när han attackerar eller om bossen missar
         Console.WriteLine("You have 50% to hit or 50% to miss");
         int chance = generator.Next(1);
 
 
-
-        if (chance == 0)
+        if (chance == 0)           // Vad som händer om bossen attackerar 
         {
             Console.WriteLine("Man you are too slow");
             Thread.Sleep(1500);
@@ -35,7 +35,7 @@ public class ToolBox
             Console.WriteLine($"The boss has {BossHP} and you have {playerHp}");
         }
 
-        else
+        else    // Vad som annars händer om du gör skada till bossen
         {
             Console.WriteLine("You damaged the boss");
             Thread.Sleep(1500);
@@ -44,22 +44,22 @@ public class ToolBox
         }
 
 
-        return (BossHP, playerHp);
+        return (BossHP, playerHp);  //Uppdaterar HP för våda bossen och playern
     }
 
-    public static (int, int) Mace(int Boss1HP, int playerHp)
+    public static (int, int) Mace(int Boss1HP, int playerHp)  //Metod som säger vad Mace ska göra och vad bossen ska göra 
     {
-        int MACEDMG = 25;
+        int MACEDMG = 300;
         int BossDMG = 15;
 
 
 
-        Random genaretor1 = new Random();
+        Random genaretor1 = new Random();       //gör Random
         Console.WriteLine("You have 50% to hit and 50% to miss");
-        int chance2 = genaretor1.Next(1);
+        int chance2 = genaretor1.Next(1);       // gör en 50/50 på det
 
 
-        if (chance2 == 1)
+        if (chance2 == 1)     //Om du missar så gör bossen skada på player
         {
             Console.WriteLine("Man you are way too slow with the mace");
             Thread.Sleep(1500);
@@ -71,20 +71,20 @@ public class ToolBox
 
 
 
-        else
+        else   //När bossen gör skada 
         {
             Console.WriteLine("You have damaged the boss");
             Thread.Sleep(1500);
             Boss1HP -= MACEDMG;
-            Console.WriteLine($"You have {playerHp} and the boss har {Boss1HP}");
+            Console.WriteLine($"You have {playerHp} and the boss has {Boss1HP}");  // vissar vad värden på dem nya HP är
         }
 
-        return (Boss1HP,playerHp);
+        return (Boss1HP, playerHp);    //Uppdaterar Hp för nästa level 
 
     }
 
 
-    public static (int,int) Scythe(int Boss1HP, int playerHp)
+    public static (int, int) Scythe(int Boss1HP, int playerHp)   //Metod som säger vad Scythe ska göra och vad bossen ska göra
     {
 
         int ScytheDMG = 75;
@@ -114,12 +114,25 @@ public class ToolBox
             Console.WriteLine($"You have {playerHp}and the boss has {Boss1HP}");
         }
 
-        return (Boss1HP,playerHp);
+        return (Boss1HP, playerHp);
 
 
     }
 
 
+    public static string Try()     //Om du dör så ska du gå tillbaka till början
+    {
+        Console.WriteLine("You have died");
+        Console.WriteLine("");
+        Thread.Sleep(1500);
+        Console.WriteLine("Do you want to try again? or give up?");
+        Console.WriteLine("");
+        Thread.Sleep(1500);
+        Console.WriteLine("please choose 1 or 2");
+        Console.WriteLine("1: try again    2: give up");
+
+        return Console.ReadLine();      //Tar imot svaret och beroende på vad det är så går den till början eller slutar spelt
+    }
 
 
 }
